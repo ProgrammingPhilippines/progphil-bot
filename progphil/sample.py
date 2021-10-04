16206ab02f3077403ec5ecd7f5e7ad2fc4b6f088
Genesis Ivan Ilagan <ivanilagan1109@gmail.com>
2021-10-04 23:10:26 +0800

    Initial Project Dependency Setup

---
+++ progphil/sample.py
@@ -0,0 +1,13 @@
+import discord
+
+
+# NOTE: Just a sample code directly from discord api documentation
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run('my token goes here')
