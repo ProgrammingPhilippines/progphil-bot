41a582819f1a5df0ebd87c4df556a72008b551be
CunningBard <61928294+CunningBard@users.noreply.github.com>
2023-01-12 23:28:26 +0800

    Announcements with media (#32)
    * basic bot run, and changed directory to the right directory
    
    * initial parts of the announcement command with media
    
    * deleted since not a cog, causes problems
    
    * add pycache and cache file extensions to gitignore
    
    * Working announcement function that needs a Media as args
    
    * tree sync to show slash commands, and changed the path to outside the bot folder
    
    * add a little comment
    
    * i didnt notice this :sob:
    
    * i didnt notice this :sob:
    
    * i didnt notice this :sob:
    
    * fixed typo (fu pycharm)
    
    * added requests as dependency and Announcements now work as Jed Intended
    
    * fixed wrong dir used (im stupid)
    
    * dynamic send ext?
    
    * made required roles be global

--- bot/cogs/announcements.py
+++ bot/cogs/announcements.py
@@ -1,15 +1,68 @@
+import io
+import discord
+import requests
 from discord.ext.commands import Bot, Cog
-# from discord.app_commands import command
+from discord.app_commands import command, describe, checks
+
+ALLOWED_EXT = ["gif", "png", "jpeg", "jpg"]
+REQUIRED_ROLES = [
+    "Admin",
+    "Moderator",
+    "Helper",
+    "baby, ako na lang kasi"
+]
+# might be exploited
 
 
 class Announcements(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
-    # Add announcement commands here
 
-    # @command(name=..., description=...)
-    #     async def ...(self, interaction: Interaction):
-    #         ...
+    # Add announcement commands here
+    @command(
+        name="media_announce",
+        description="Send a announcement with media",
+    )
+    @describe(
+        photo_link="The Link to the Photo",
+        to_announce="Message to announce"
+    )
+    @checks.has_any_role(
+        *REQUIRED_ROLES
+    )
+    async def announce_with_media(
+            self,
+            interaction: discord.Interaction,
+            photo_link: str,
+            to_announce: str,
+    ) -> None:
+        """
+         Announcement method that needs a link to a photo as an Argument,
+         To use the command you must provide a link to the photo you want to send,
+         and a single line you want to send
+        """
+        seperated_photo_link = photo_link.split(".")
+        if len(seperated_photo_link) == 0:  # errors when no dots in the given argument
+            return await interaction.response.send_message(
+                "Provided link is Invalid",
+                ephemeral=True
+            )
+        elif seperated_photo_link[-1] not in ALLOWED_EXT:   # errors when the ext is not allowed
+            return await interaction.response.send_message(
+                f"Provided link is not allowed, must be a file that ends with {ALLOWED_EXT}",
+                ephemeral=True
+            )
+
+        request_file = requests.get(photo_link)
+        if request_file.status_code != 200:
+            return await interaction.response.send_message(
+                "Provided link is either not working or Internal Sever Error",
+                ephemeral=True
+            )
+
+        with io.BytesIO(request_file.content) as file:  # converts to file-like object
+            channel = self.bot.get_channel(interaction.channel_id)
+            await channel.send(to_announce, file=discord.File(file, f"image_gihapon.{seperated_photo_link[-1]}"))
 
 
 async def setup(bot: Bot):
