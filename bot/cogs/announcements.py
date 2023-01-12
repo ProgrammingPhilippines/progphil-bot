333647784072962ae872c101ba9004cb8edbd10a
Eric <jhonjhonjumaday@gmail.com>
2023-01-12 20:22:25 +0800

    Working announcement function that needs a Media as args

--- bot/cogs/announcements.py
+++ bot/cogs/announcements.py
@@ -1,14 +1,53 @@
-from discord.ext.commands import Bot, Cog, Context
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+ALLOWED_EXT = ["gif", "png", "jpeg", "jpg"]
 
 
-class Announcements(Cog):
-    def __init__(self, bot: Bot):
+class Announcements(commands.Cog):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     # Add announcement commands here
-    async def announce_with_media(self, ctx: Context):
-        pass
+    @app_commands.command(
+        name="Media Announce",
+        description="Send A announcement with media",
+    )
+    @app_commands.describe(
+        photo_link="The Link to the Photo",
+        to_announce="Message to announce"
+    )
+    @app_commands.checks.has_any_role(
+        "Admin",
+        "Moderator",
+        "Helper",
+        "baby, ako na lang kasi"
+    )
+    async def announce_with_media(
+            self,
+            interaction: discord.Interaction,
+            photo_link: str,
+            to_announce: str,
+    ) -> None:
+        """
+         Announcement method that needs a link to a photo as an Argument,
+         To use the command you must provide a link to the photo you want to send,
+         and a single ine you want to send
+        """
+        seperated_photo_link = photo_link.split(".")
+        if len(seperated_photo_link) == 0:
+            return await interaction.response.send_message("Provided link is Invalid")
+        elif seperated_photo_link[-1] not in ALLOWED_EXT:
+            return await interaction.response.send_message(
+                f"Provided link is not allowed, must be a file that ends with {ALLOWED_EXT}"
+            )
+
+        channel = self.bot.get_channel(interaction.channel_id)
+
+        await channel.send(to_announce)
+        await channel.send(photo_link)
 
 
-def setup(bot: Bot):
-    bot.add_cog(Announcements(bot))
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Announcements(bot))
