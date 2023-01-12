c9cd7e70e593a82edac93e5620cd78c6e9b8cc1b
CunningBard <61928294+CunningBard@users.noreply.github.com>
2023-01-12 23:28:26 +0800

    Announcements with media (#32)
    * basic bot run, and changed directory to the right directory
    
    * initial parts of the announcement command with media
    
    * deleted since not a cog, causes problems
    
    * add pycache and cache file extensions to gitignore
    
    * Working announcement function that needs a Media as args
    
    * tree sync to show slash commands, and changed the path to outside the bot folder
    
    * add a little comment
    
    * i didnt notice this :sob:
    
    * i didnt notice this :sob:
    
    * i didnt notice this :sob:
    
    * fixed typo (fu pycharm)
    
    * added requests as dependency and Announcements now work as Jed Intended
    
    * fixed wrong dir used (im stupid)
    
    * dynamic send ext?
    
    * made required roles be global

--- bot/main.py
+++ bot/main.py
@@ -5,10 +5,10 @@ from discord.ext.commands import Bot
 from dotenv import load_dotenv
 
 
-load_dotenv('.env')  # Loads .env file
-token = os.getenv('token')
+load_dotenv(".env")  # Loads .env file
+token = os.getenv("token")
 intents = Intents().all()
-intents.dm_messages = False
+intents.dm_messages = False  # pycharm showing a warning Intents' object attribute 'dm_messages' is read-only
 
 
 class ProgPhil(Bot):
@@ -17,24 +17,26 @@ class ProgPhil(Bot):
             **kwargs,
             command_prefix="?",
             intents=intents
-            )
-    
+        )
+
     async def on_ready(self) -> None:
         """Invoked when the bot finish setting up
         
         This can get invoked multiple times, use :meth:`setup_hook()` instead
         for loading databases, etc."""
 
-        print(f'{self.user.display_name} running.')
+        print(f"{self.user.display_name} running.")
 
     async def setup_hook(self) -> None:
         """This method only gets called ONCE, load stuff here."""
         # Load every cog inside cogs folder.
         for cog in os.listdir("bot/cogs"):
             if cog[-3:] == ".py":
-                await self.load_extension(f'cogs.{cog[:-3]}')
+                await self.load_extension(f"cogs.{cog[:-3]}")
+
         await self.tree.sync()
 
 
-bot = ProgPhil()
-bot.run(token)
+if __name__ == '__main__':
+    bot = ProgPhil()
+    bot.run(token)
