d9fd9ec363ea7c1ec3de98195b8014f4dc0808a3
Eric <jhonjhonjumaday@gmail.com>
2023-01-12 11:10:03 +0800

    initial parts of the announcement command with media

--- bot/cogs/announcements.py
+++ bot/cogs/announcements.py
@@ -1,11 +1,14 @@
-from discord.ext.commands import Bot, Cog
+from discord.ext.commands import Bot, Cog, Context
 
 
 class Announcements(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
+
     # Add announcement commands here
+    async def announce_with_media(self, ctx: Context):
+        pass
 
 
-async def setup(bot: Bot):
+def setup(bot: Bot):
     bot.add_cog(Announcements(bot))
