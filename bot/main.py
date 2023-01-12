c4997ee51bba39fcf7da33d223ad15dd0bc31ec2
Jedddy <jedbalita25@gmail.com>
2023-01-12 09:59:09 +0800

    initial codebase

---
+++ bot/main.py
@@ -0,0 +1,35 @@
+import os
+
+from discord import Intents
+from discord.ext.commands import Bot
+from dotenv import load_dotenv
+
+
+load_dotenv("token.env") # Loads token.env file
+token = os.getenv("token")
+intents = Intents().all()
+intents.dm_messages = False
+
+
+class ProgPhil(Bot):
+    def __init__(self, **kwargs):
+        super().__init__(
+            **kwargs,
+            command_prefix="?",
+            intents=intents
+            )
+    
+    async def on_ready(self) -> None:
+        """Invoked when the bot finish setting up
+        
+        This can get invoked multiple times, use :meth:`setup_hook()` instead
+        for loading databases, etc."""
+
+        print(f"{self.user.display_name} running.")
+
+    async def setup_hook(self) -> None:
+        """This method only gets called ONCE, load stuff here."""
+        # Load every cog inside cogs folder.
+        for cog in os.listdir("bot/cogs"):
+            if cog[-3:] == ".py":
+                await self.load_extension(f"cogs.{cog[:-3]}")
