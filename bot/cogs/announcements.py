c828ac8939724623f7eee9f53a73b52ae755cea3
tora-o <97511605+tora-o@users.noreply.github.com>
2023-01-12 21:01:00 +0800

    Codebase fixes (#31)
    * added requirements.txt
    
    * edited main.py
    
    * deleted cogs init and edited announcements.py
    
    * add template for slash commands

--- bot/cogs/announcements.py
+++ bot/cogs/announcements.py
@@ -1,4 +1,5 @@
 from discord.ext.commands import Bot, Cog
+# from discord.app_commands import command
 
 
 class Announcements(Cog):
@@ -6,6 +7,10 @@ class Announcements(Cog):
         self.bot = bot
     # Add announcement commands here
 
+    # @command(name=..., description=...)
+    #     async def ...(self, interaction: Interaction):
+    #         ...
+
 
 async def setup(bot: Bot):
-    bot.add_cog(Announcements(bot))
+    await bot.add_cog(Announcements(bot))
