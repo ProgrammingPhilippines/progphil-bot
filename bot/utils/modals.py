d634fb40dc24a62bc625463705479ba21d93d6e4
tora-o <97511605+tora-o@users.noreply.github.com>
2023-01-13 15:54:44 +0800

    created modals.py

---
+++ bot/utils/modals.py
@@ -0,0 +1,29 @@
+from discord import Interaction, TextStyle, Attachment
+from discord.ui import Modal, TextInput
+
+class Announcement(Modal, title='Announcement'):
+    announcement_title = TextInput(
+        label='Title',
+        placeholder='Title',
+        required=True,
+    )
+    announcement = TextInput(
+        label='Announcement',
+        placeholder='Announcement',
+        required=True,
+        style=TextStyle.paragraph
+    )
+
+    def __init__(self, interaction: Interaction, attachment: Attachment):
+        super().__init__()
+        self.interaction = interaction
+        self.attachment = attachment
+
+    async def on_submit(self, interaction: Interaction) -> None:
+        channel = interaction.channel
+        photo = await self.attachment.to_file()
+        await channel.send(self.announcement_title.value, file=photo)
+        await channel.send(self.announcement.value)
+        await interaction.response.send_message(
+            'Announcement has been sent', ephemeral=True
+        )
