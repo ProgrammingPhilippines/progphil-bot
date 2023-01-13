a9752bb2df77a449578295d0a93f7f07a79e2440
tora-o <97511605+tora-o@users.noreply.github.com>
2023-01-13 18:21:48 +0800

    Multiline with media (#37)
    * created modals.py
    
    * added multiline with media announcement command
    
    * created a method to check if the passed attachment is allowed
    
    * made the photo optional
    
    * documentation updates
    
    * finished multiline announcements with media
    
    * removed walrus operator

--- bot/cogs/announcements.py
+++ bot/cogs/announcements.py
@@ -1,6 +1,10 @@
+from typing import Optional
+
 import discord
 from discord.ext.commands import Bot, Cog
 from discord.app_commands import command, describe, checks
+
+from bot.utils.modals import Announcement
 
 ALLOWED_EXT = ["gif", "png", "jpeg", "jpg"]
 REQUIRED_ROLES = [
@@ -9,7 +13,18 @@ REQUIRED_ROLES = [
     "Helper",
     "baby, ako na lang kasi"
 ]
+
+
 # might be exploited
+
+def is_allowed(attachment: discord.Attachment):
+    """
+    Checks if the attachment is allowed
+
+    :param attachment: attachment to check
+    :return:
+    """
+    return attachment.filename.split(".")[-1] in ALLOWED_EXT
 
 
 class Announcements(Cog):
@@ -37,8 +52,13 @@ class Announcements(Cog):
         """
         Announcement Command that needs a Photo as an Argument and a one line message,
         you need to have the right perms inorder to run this command
+
+        :param interaction:
+        :param photo:
+        :param to_announce:
         """
-        if photo.filename.split(".")[-1] not in ALLOWED_EXT:
+
+        if not is_allowed(photo):
             return await interaction.response.send_message(
                 f"File {photo.filename}, is not a supported file, only send photos with {ALLOWED_EXT}",
                 ephemeral=True
@@ -59,13 +79,51 @@ class Announcements(Cog):
     @checks.has_any_role(
         *REQUIRED_ROLES
     )
-    async def announce(self, interactions: discord.Interaction, message: str):
+    async def announce(self, interactions: discord.Interaction, message: str) -> None:
+        """
+        Announcement Command that needs a one line message
+
+        :param interactions:
+        :param message:
+        """
         channel = interactions.channel  # get the channel the command was called on
         await interactions.response.send_message(
             f"Announcement has been made",
             ephemeral=True
         )
         await channel.send(message)
+
+    @command(
+        name="multi_with_media",
+        description="Make Multiple Line Announcements with Media"
+    )
+    @checks.has_any_role(
+        *REQUIRED_ROLES
+    )
+    @describe(
+        photo="The Photo File"
+    )
+    async def multi_with_media(
+            self,
+            interaction: discord.Interaction,
+            photo: Optional[discord.Attachment] = None,
+    ) -> None:
+        """
+        Announcement Command that uses modals to make announcements with media
+
+        :param interaction:
+        :param photo:
+        """
+
+        if photo and not is_allowed(photo):
+            return await interaction.response.send_message(
+                f"File {photo.filename}, is not a supported file, only send photos with {ALLOWED_EXT}",
+                ephemeral=True
+            )
+
+        announcement = Announcement(photo)
+        await interaction.response.send_modal(announcement)
+        await announcement.wait()
 
 
 async def setup(bot: Bot):
