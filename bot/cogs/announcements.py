4418997dfa64b60e1f310ac9ee163050a4bc0973
CunningBard <61928294+CunningBard@users.noreply.github.com>
2023-01-13 14:14:56 +0800

    Media announce better method (#35)
    * basic bot run, and changed directory to the right directory
    
    * initial parts of the announcement command with media
    
    * deleted since not a cog, causes problems
    
    * add pycache and cache file extensions to gitignore
    
    * Working announcement function that needs a Media as args
    
    * tree sync to show slash commands, and changed the path to outside the bot folder
    
    * add a little comment
    
    * i didnt notice this :sob:
    
    * i didnt notice this :sob:
    
    * i didnt notice this :sob:
    
    * fixed typo (fu pycharm)
    
    * added requests as dependency and Announcements now work as Jed Intended
    
    * fixed wrong dir used (im stupid)
    
    * dynamic send ext?
    
    * made required roles be global
    
    * use a better method
    
    * remove unused import and add tab since pycharm hates it
    
    * remove unused import
    
    * improve command, added the file extension checker and respond when everything goes right
    
    * added a small Note

--- bot/cogs/announcements.py
+++ bot/cogs/announcements.py
@@ -1,9 +1,6 @@
-import io
 import discord
-import requests
 from discord.ext.commands import Bot, Cog
 from discord.app_commands import command, describe, checks
-from discord import app_commands
 
 ALLOWED_EXT = ["gif", "png", "jpeg", "jpg"]
 REQUIRED_ROLES = [
@@ -25,7 +22,7 @@ class Announcements(Cog):
         description="Send a announcement with media",
     )
     @describe(
-        photo_link="The Link to the Photo",
+        photo="The Photo File",
         to_announce="Message to announce"
     )
     @checks.has_any_role(
@@ -34,50 +31,42 @@ class Announcements(Cog):
     async def announce_with_media(
             self,
             interaction: discord.Interaction,
-            photo_link: str,
+            photo: discord.Attachment,
             to_announce: str,
     ) -> None:
         """
-         Announcement method that needs a link to a photo as an Argument,
-         To use the command you must provide a link to the photo you want to send,
-         and a single line you want to send
+        Announcement Command that needs a Photo as an Argument and a one line message,
+        you need to have the right perms inorder to run this command
         """
-        seperated_photo_link = photo_link.split(".")
-        if len(seperated_photo_link) == 0:  # errors when no dots in the given argument
+        if photo.filename.split(".")[-1] not in ALLOWED_EXT:
             return await interaction.response.send_message(
-                "Provided link is Invalid",
-                ephemeral=True
-            )
-        elif seperated_photo_link[-1] not in ALLOWED_EXT:   # errors when the ext is not allowed
-            return await interaction.response.send_message(
-                f"Provided link is not allowed, must be a file that ends with {ALLOWED_EXT}",
+                f"File {photo.filename}, is not a supported file, only send photos with {ALLOWED_EXT}",
                 ephemeral=True
             )
 
-        request_file = requests.get(photo_link)
-        if request_file.status_code != 200:
-            return await interaction.response.send_message(
-                "Provided link is either not working or Internal Sever Error",
-                ephemeral=True
-            )
+        channel = self.bot.get_channel(interaction.channel_id)
+        await channel.send(to_announce, file=await photo.to_file())
+        await interaction.response.send_message(
+            f"Announcement Has been sent",
+            ephemeral=True
+        )
 
-        with io.BytesIO(request_file.content) as file:  # converts to file-like object
-            channel = self.bot.get_channel(interaction.channel_id)
-            await channel.send(to_announce, file=discord.File(file, f"image_gihapon.{seperated_photo_link[-1]}"))
     @command(
-       name="announce",
-       description="Make Single Line Announcements on the Channel it was called on"
-    ) 
+        name="announce",
+        description="Make Single Line Announcements on the Channel it was called on"
+    )
     @describe(message="The Announcement Message to send")
     @checks.has_any_role(
         *REQUIRED_ROLES
     )
     async def announce(self, interactions: discord.Interaction, message: str):
-        channel = interactions.channel # get the channel the command was called on
-        await interactions.response.send_message(f"Announcement has been made", ephemeral=True) 
+        channel = interactions.channel  # get the channel the command was called on
+        await interactions.response.send_message(
+            f"Announcement has been made",
+            ephemeral=True
+        )
         await channel.send(message)
+
 
 async def setup(bot: Bot):
     await bot.add_cog(Announcements(bot))
-
-    
