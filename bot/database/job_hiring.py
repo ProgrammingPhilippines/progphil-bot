3e7a69d15cf189a31e9b38bd4e76378d0e52636d
tora-o <97511605+tora-o@users.noreply.github.com>
2023-03-18 15:54:12 +0800

    feat: created db handler for job_hiring

---
+++ bot/database/job_hiring.py
@@ -0,0 +1,22 @@
+from asyncpg import Pool
+
+
+class JobHiringDB:
+    def __int__(self, pool: Pool):
+        self._pool = pool
+
+    def get_config(self) -> dict:
+        async with self._pool.acquire() as conn:
+            conn: Pool
+
+            config = await conn.fetchrow("""
+                SELECT * FROM pph_job_hiring;
+            """)
+
+        return dict(config) if config is not None else None
+
+    def update(self, channel_id: int, schedule: str) -> None:
+        ...
+
+    def insert(self, channel_id: int, schedule: str) -> None:
+        ...
