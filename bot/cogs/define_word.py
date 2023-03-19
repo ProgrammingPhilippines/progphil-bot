fad488d86e05baa3e7b54cd18a102d798c4585e4
jovelluna <jchrister718@gmail.com>
2023-03-19 15:36:04 +0800

    added toggle

--- bot/cogs/define_word.py
+++ bot/cogs/define_word.py
@@ -1,14 +1,18 @@
 import requests
 import discord
-from discord.ext.commands import Bot, Cog
-from discord.app_commands import Choice, choices, command, describe
+from discord.ext.commands import Bot
+from discord.app_commands import command, describe
 from utils.decorators import is_staff 
+from discord.ext.commands import GroupCog
 
-class defineWord(Cog):
+
+command_enabled = True
+
+class define(GroupCog):
     def __init__ (self, bot: Bot):
         self.bot = bot
 
-    @command(name="define",
+    @command(name="word",
              description="Give Dictionary Definition to the Given Word")
     @describe(word="The Word that will be Defined")
     async def define(
@@ -22,6 +26,10 @@ class defineWord(Cog):
         :param interaction: Interaction
         :param word: The Word to Define
         """
+        global command_enabled
+        if command_enabled==False:
+            await interaction.response.send_message("Sorry, this command is currently disabled.")
+            return
         
         url = "https://api.dictionaryapi.dev/api/v2/entries/en/"+word
         response=requests.get(url)
@@ -33,7 +41,30 @@ class defineWord(Cog):
             definition=output['meanings'][0]['definitions'][0]['definition']
             respond_message="Word: "+word+"\nDefinition: "+str(definition)
         await interaction.response.send_message(respond_message)
+
+    @is_staff()
+    @command(name="toggle",
+             description="Turn Define Command On/Off")
+    async def defineOnOff(
+        self,
+        interaction: discord.Interaction,
+    ) -> None:
+        """
+        Turn Define Command On/Off
+
+        :param interaction: Interaction
+        """
+        global command_enabled
+
+        if command_enabled==True:
+            command_enabled= False
+            response="Command is now Disabled"
+        else:
+            command_enabled=True
+            response="Command is now Enabled"
+
+        await interaction.response.send_message(response)
         
 
 async def setup(bot: Bot):
-    await bot.add_cog(defineWord(bot))
+    await bot.add_cog(define(bot))
