b8de0cbd455b8fc5fcb9c61703bc8544510a49e3
jovelluna <jchrister718@gmail.com>
2023-03-25 18:40:42 +0800

    multi pages

--- bot/cogs/define_word.py
+++ bot/cogs/define_word.py
@@ -1,5 +1,6 @@
 import requests
 import discord
+from discord.ui import Button, View
 from discord import Embed
 from discord.ext.commands import Bot
 from discord.app_commands import command, describe
@@ -33,7 +34,6 @@ class Define(GroupCog):
 
         url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
         response = requests.get(url)
-
         if "title" in response.json() and response.json()["title"] == "No Definitions Found":
             message = response.json()['message']
             respond_message = Embed(
@@ -41,8 +41,11 @@ class Define(GroupCog):
                 description=message,
                 color=discord.Color.blurple()
             )
+            await interaction.response.send_message(embed=respond_message) 
+            return
         else:
             definitions = ''
+            defs = []
             num = 1
             for words in response.json():
                 for meaning in words['meanings']:
@@ -50,16 +53,53 @@ class Define(GroupCog):
                         part_of_speech = meaning['partOfSpeech']
                         definitions += f'({str(part_of_speech)}) ' + \
                             str(num)+'. '+definition['definition'] + '\n'
+                        defs.append(f'({str(part_of_speech)}) ' +
+                                    str(num)+'. '+definition['definition'] + '\n')
                         num += 1
+        global index_count
+        index_count = 0
 
-            message = f"Here's what i got on the Word {word} \n\n {definitions}"
+        def create_embed(mode):
+            global index_count
+
+            if mode == 'next':
+                index_count += 1
+            elif mode == 'previous':
+                index_count -= 1
+
+            if index_count >= len(defs):
+                index_count = -1
+            elif index_count < 0:
+                index_count = 0
+
+            message = f"Here's what i got on the Word {word} \n\n {defs[index_count]}"
             respond_message = Embed(
                 title=word,
                 description=message,
                 color=discord.Color.blurple()
             )
 
-        await interaction.response.send_message(embed=respond_message)
+            return respond_message
+
+        async def callback_next(interaction):
+            respond_message = create_embed('next')
+            await interaction.response.edit_message(embed=respond_message, view=view)
+
+        async def callback_previous(interaction):
+            respond_message = create_embed('previous')
+            await interaction.response.edit_message(embed=respond_message, view=view)
+
+        button_next = Button(
+            label='Next', style=discord.ButtonStyle.green, emoji='➡')
+        button_previous = Button(
+            label='Previous', style=discord.ButtonStyle.green, emoji='⬅')
+        button_next.callback = callback_next
+        button_previous.callback = callback_previous
+        view = View()
+        view.add_item(button_previous)
+        view.add_item(button_next)
+
+        await interaction.response.send_message(embed=create_embed('first'), view=view)
 
     # Command to Turn this Command On or Off
     @is_staff()
