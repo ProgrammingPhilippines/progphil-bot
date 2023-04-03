c2685a78cca6aa4f8d9659050b34067b91302c50
tora-o <97511605+tora-o@users.noreply.github.com>
2023-04-03 16:10:30 +0800

    enhancement: updated modals for hiring

--- bot/ui/modals/job_hiring.py
+++ bot/ui/modals/job_hiring.py
@@ -3,6 +3,7 @@ from discord.ui import Modal, TextInput
 
 
 class OncePerDay(Modal):
+    schedule_type = 0
     title = "Every Day At A Specific Time."
     sched = TextInput(
         label="Input the desired time.",
@@ -16,6 +17,7 @@ class OncePerDay(Modal):
 
 
 class SpecificDate(OncePerDay):
+    schedule_type = 1
     title = "Once On A Specific Day"
     sched = TextInput(
         label="Input the desired date.",
@@ -25,6 +27,7 @@ class SpecificDate(OncePerDay):
 
 
 class Recurring(OncePerDay):
+    schedule_type = 2
     title = "Every N [months|days|hours|minutes|seconds]"
     sched = TextInput(
         label="Input the desired schedule.",
