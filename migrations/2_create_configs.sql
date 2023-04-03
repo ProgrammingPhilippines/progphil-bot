099774ce5f6e2c9f6357cbb5c0a192d1631ea9d9
tora-o <97511605+tora-o@users.noreply.github.com>
2023-04-03 14:27:54 +0800

    chore: added migration for job hiring config

--- migrations/2_create_configs.sql
+++ migrations/2_create_configs.sql
@@ -3,5 +3,17 @@ CREATE TABLE IF NOT EXISTS pph_config_auto (
     config_status BOOLEAN DEFAULT true
 );
 
-INSERT INTO pph_config_auto(config_type) VALUES ('auto_responder');
-INSERT INTO pph_config_auto(config_type) VALUES ('auto_tagging');
+INSERT INTO pph_config_auto(config_type)
+SELECT 'auto_responder'
+    WHERE NOT EXISTS
+        (SELECT 1 FROM pph_config_auto WHERE config_type = 'auto_responder');
+
+INSERT INTO pph_config_auto(config_type)
+SELECT 'auto_tagging'
+    WHERE NOT EXISTS
+        (SELECT 1 FROM pph_config_auto WHERE config_type = 'auto_tagging');
+
+INSERT INTO pph_config_auto(config_type)
+SELECT 'job_hiring'
+    WHERE NOT EXISTS
+        (SELECT 1 FROM pph_config_auto WHERE config_type = 'job_hiring');
