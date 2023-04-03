1c7bb755296610addbe363d3f428a5b83e8f2306
tora-o <97511605+tora-o@users.noreply.github.com>
2023-04-03 16:54:26 +0800

    feat: created migration for job hiring

---
+++ migrations/4_job_hiring.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS pph_job_hiring (
+    channel_id BIGINT,
+    schedule VARCHAR(70),
+    schedule_type INTEGER
+);
+
+INSERT INTO pph_config_auto(config_type) VALUES ('job_hiring');
