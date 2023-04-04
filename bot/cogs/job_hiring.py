fb8b59e204a6bb3e274ecc9301fbcf71726efdde
Jedddy <jedbalita25@gmail.com>
2023-04-05 01:20:01 +0800

    feat: add the loop

--- bot/cogs/job_hiring.py
+++ bot/cogs/job_hiring.py
@@ -1,10 +1,11 @@
+from datetime import datetime, timedelta
 from textwrap import dedent
 from typing import Union
 
 import cloudscraper
-import discord
 from bs4 import BeautifulSoup
-from discord import Embed, Interaction
+from discord import Embed, Interaction, Color, TextChannel, utils
+from discord.ext import tasks
 from discord.app_commands import command, describe
 from discord.ext.commands import Bot, GroupCog
 
@@ -18,6 +19,7 @@ from ui.modals.job_hiring import (
 )
 from ui.views.job_hiring import JobConfig
 from utils.decorators import is_staff
+from utils.utils import parse
 
 (
     DAILY,
@@ -37,12 +39,80 @@ class Hiring(GroupCog):
 
     def __init__(self, bot: Bot):
         self.bot = bot
-        self.db: Union[JobHiringDB, None] = None
-        self._toggle_config: Union[Config, None] = None
-
-    async def cog_load(self) -> None:
         self.db = JobHiringDB(self.bot.pool)
         self._toggle_config = Config(self.bot.pool)
+        self.time_schedule: Union[str, None] = None
+        self.schedule_type: Union[int, None] = None
+        self.toggle: Union[bool, None] = None
+
+    async def _refresh(self):
+        # Stop the on going loops.
+        if self.hiring_task.is_running():
+            self.hiring_task.cancel()
+        if self.hiring_task2.is_running():
+            self.hiring_task2.cancel()            
+
+        config = await self.db.get_config()
+        self.toggle = await self._toggle_config.get_config("job_hiring")
+
+        if not config:
+            return
+
+        self.time_schedule = config["schedule"]
+        self.schedule_type = config["schedule_type"]
+        self.hiring_channel = config["channel_id"]
+
+        if self.schedule_type == 0:
+            # 1 Means daily.
+            utc8 = datetime.strptime(
+                self.time_schedule, 
+                "%H:%M"
+            ).time()
+
+            sched_today = datetime.combine(
+                datetime.today(),
+                utc8
+            )
+
+            schedule = sched_today - timedelta(hours=8)  # Converts the schedule to UTC+0
+            self.hiring_task.change_interval(time=schedule.time())
+            task = self.hiring_task
+        else:
+            task = self.hiring_task2
+
+        task.start()
+
+    async def cog_load(self) -> None:
+        await self._refresh()
+
+    @tasks.loop()
+    async def hiring_task(self):
+        if not self.toggle["config_status"]:
+            return
+
+        await self.send_job_hiring()
+
+    @tasks.loop()
+    async def hiring_task2(self):
+        if not self.toggle["config_status"]:
+            return
+
+        if self.schedule_type == 1:
+            # 1 means ONCE on a set date, once done, it 
+            # Will be deleted from the database.
+            schedule = datetime.fromtimestamp(int(self.time_schedule))
+            await utils.sleep_until(schedule)
+            await self.send_job_hiring()
+            await self.db.delete(self.hiring_channel)
+            await self._refresh()
+        else:
+            # This is if the schedule is 2, which is recurring.
+            # Parse the schedule to seconds, and then sleep and send hiring and repeat.
+
+            parsed = parse(self.time_schedule)
+            schedule = datetime.now() + timedelta(seconds=parsed)
+            await utils.sleep_until(schedule)
+            await self.send_job_hiring()
 
     async def send_job_hiring(self):
         scraper = cloudscraper.create_scraper()
@@ -72,8 +142,15 @@ class Hiring(GroupCog):
                 .replace('Job Type', '')  # Full-time
             )
 
-            # will be changed to the actual channel
-            await log_channel.send(
+            if not self.hiring_channel:
+                return
+
+            channel = self.bot.get_channel(self.hiring_channel)
+
+            if not channel:
+                return
+
+            await channel.send(
                 embed=Embed(
                     title='Job Hiring',
                     description=dedent(f"""
@@ -117,7 +194,7 @@ class Hiring(GroupCog):
                         Schedule: {schedule_config["schedule"]}
                         Schedule Type: {schedule_map[schedule_config["schedule_type"]]}
                     """),
-            color=discord.Color.random()
+            color=Color.random()
         )
 
         await interaction.response.send_message(
@@ -139,11 +216,12 @@ class Hiring(GroupCog):
             f"Turned {toggle_map[toggle]} Hiring.",
             ephemeral=True
         )
+        await self._refresh()
 
     @is_staff()
     @command(name="setup", description="Setup the trivia")
     @describe(channel="Channel to send the trivia to")
-    async def setup(self, interaction: discord.Interaction, channel: discord.TextChannel) -> None:
+    async def setup(self, interaction: Interaction, channel: TextChannel) -> None:
         """
         Sets up the job hiring cog
 
@@ -160,11 +238,12 @@ class Hiring(GroupCog):
             return
 
         await self._send_view(interaction, channel, "setup")
+        await self._refresh()
 
     @is_staff()
     @command(name="channel", description="Change the channel to send the job hiring to")
     @describe(channel="Channel to send the job hiring to")
-    async def channel(self, interaction: Interaction, channel: discord.TextChannel) -> None:
+    async def channel(self, interaction: Interaction, channel: TextChannel) -> None:
         """
         Changes the channel to send the job hiring to
 
@@ -190,6 +269,7 @@ class Hiring(GroupCog):
             f"Changed the channel to {channel.mention}",
             ephemeral=True
         )
+        await self._refresh()
 
     @is_staff()
     @command(name="schedule", description="Change the schedule of the job hiring")
@@ -210,7 +290,7 @@ class Hiring(GroupCog):
 
         await self._send_view(interaction, self.bot.get_channel(int(schedule_config["channel_id"])), "schedule")
 
-    async def _send_view(self, interaction: Interaction, channel: discord.TextChannel, sched_type: str):
+    async def _send_view(self, interaction: Interaction, channel: TextChannel, sched_type: str):
         embed = Embed(
             title="Job Hiring Schedule",
             description=dedent("""
@@ -218,9 +298,12 @@ class Hiring(GroupCog):
             :two: - Once on a specific day
             :three: - Every N [months|days|hours|minutes|seconds]
             """),
-            color=discord.Color.random()
+            color=Color.random()
         )
-        await interaction.response.send_message(embed=embed, view=JobConfig(self.bot.pool, channel, sched_type))
+        view = JobConfig(self.bot.pool, channel, sched_type)
+        await interaction.response.send_message(embed=embed, view=view)
+        await view.wait()
+        await self._refresh()
 
 
 async def setup(bot: Bot):
