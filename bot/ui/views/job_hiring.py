5bc7a1a81ed0ddef3ccc969a67a37e0202164a44
Jedddy <jedbalita25@gmail.com>
2023-04-05 01:19:49 +0800

    feat: add validators for input

--- bot/ui/views/job_hiring.py
+++ bot/ui/views/job_hiring.py
@@ -1,9 +1,10 @@
 from asyncpg import Pool
 from discord import Interaction, ButtonStyle, TextChannel
-from discord.ui import View, Button, button
+from discord.ui import View, Button, Item, button
 
 from ui.modals.job_hiring import OncePerDay, SpecificDate, Recurring
 from database.job_hiring import JobHiringDB
+from utils.utils import validate_time, validate_date, parse
 
 
 class JobConfig(View):
@@ -20,6 +21,9 @@ class JobConfig(View):
 
         await interaction.response.send_modal(modal)
         await modal.wait()
+
+        if not validate_time(modal.sched):
+            raise Exception("Please enter the correct format. HH:MM")
 
         if self.type == "setup":
             await self.db.insert(
@@ -43,16 +47,21 @@ class JobConfig(View):
         await interaction.response.send_modal(modal)
         await modal.wait()
 
+        schedule = validate_date(modal.sched)
+
+        if not schedule:
+            raise Exception("Please enter a correct format and a valid date. MM/DD/YYYY")
+
         if self.type == "setup":
             await self.db.insert(
                 channel_id=self.channel.id,
-                schedule=modal.sched,
+                schedule=str(int(schedule)),
                 schedule_type=modal.schedule_type
             )
         else:
             await self.db.update(
                 channel_id=self.channel.id,
-                schedule=modal.sched,
+                schedule=str(int(schedule)),
                 schedule_type=modal.schedule_type
             )
 
@@ -64,6 +73,9 @@ class JobConfig(View):
 
         await interaction.response.send_modal(modal)
         await modal.wait()
+
+        if not parse(modal.sched):
+            raise Exception("Enter a valid schedule.")
 
         if self.type == "setup":
             await self.db.insert(
@@ -85,4 +97,8 @@ class JobConfig(View):
 
         del self
 
-    # TODO: Make a regex validator on modal instance variable after submitting.
+    async def on_error(self, interaction: Interaction, error: Exception, item: Item) -> None:
+        await interaction.followup.send(
+            content=error,
+            ephemeral=True
+        )
