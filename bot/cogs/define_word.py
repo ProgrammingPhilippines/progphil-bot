5419b8c774f82cd70ae73f17cf740037961c1e81
tora-o <97511605+tora-o@users.noreply.github.com>
2023-04-14 22:08:10 +0800

    Fix define (#132)
    * chore: code clean up
    
    * chore: code clean up
    
    * feat: created pagination for define
    
    * feat: created migration file for define_word
    
    * enhancement: implemented pagination
    
    * enhancement: refactored the toggle command
    
    * fix: fixed offsets
    
    * fix: bug fix finished
    
    * enhancement: added more error to catch
    
    * chore: added usage and help
    
    * fix: fixed to pass pylint tests

--- bot/cogs/define_word.py
+++ bot/cogs/define_word.py
@@ -1,6 +1,7 @@
+from typing import List, Tuple
+
 import requests
 import discord
-from discord.ui import Button, View
 from discord import Embed
 from discord.ext.commands import (
     Bot,
@@ -10,35 +11,54 @@ from discord.ext.commands import (
 )
 from discord.app_commands import command
 
+from database.config_auto import Config
+from ui.views.define_word import DefineWordPagination
 from utils.decorators import is_staff
 
 
 class Define(GroupCog):
     def __init__(self, bot: Bot):
         self.bot = bot
-        self.command_enabled = True
+        self.config = Config(self.bot.pool)
 
-    @prefixed_command()
+    @prefixed_command(
+        usage="<word>",
+        help="Define a Word (e.g. define hello)"
+    )
     async def define(
-        self,
-        ctx: Context,
-        word: str,
-    ) -> None:
+            self,
+            ctx: Context,
+            word: str,
+    ):
         """
         Give Dictionary Definition to the Given Word
 
         :param word: The Word to Define
+        :param ctx: The Context of the Command
         """
 
-        if not self.command_enabled:
-            await ctx.send("Sorry, this command is currently disabled.", delete_after=10)
+        config = await self.config.get_config("define_word")
+
+        if not config["config_status"]:
+            await ctx.send("Sorry, this command is currently disabled.")
             return
 
         url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
         response = requests.get(url)
 
-        if response.status_code == 404:
-            message = f"Could not find definition for {word}"
+        if not response.ok:
+            message = f"Could not find definition for {word.lower()}"
+            respond_message = Embed(
+                description=message,
+                color=discord.Color.blurple()
+            )
+
+            return await ctx.send(embed=respond_message)
+
+        data = response.json()
+
+        if "title" in data and data["title"] == "No Definitions Found":
+            message = data['message']
             respond_message = Embed(
                 title=word,
                 description=message,
@@ -47,92 +67,43 @@ class Define(GroupCog):
             await ctx.send(embed=respond_message)
             return
 
-        elif "title" in response.json() and \
-           response.json()["title"] == "No Definitions Found":
-            message = response.json()['message']
-            respond_message = Embed(
-                title=word,
-                description=message,
-                color=discord.Color.blurple()
-            )
-            await ctx.send(embed=respond_message)
-            return
-        else:
-            defs = []
-            num = 1
-            for words in response.json():
-                for meaning in words['meanings']:
-                    for definition in meaning['definitions']:
-                        part_of_speech = meaning['partOfSpeech']
-                        defs.append(f'({str(part_of_speech)}) ' +
-                                    str(num)+'. '+definition['definition'] + '\n')
-                        num += 1
-        global index_count
-        index_count = 0
+        data = self._format_data(data)
+        part_of_speech, definition = data[0]
 
-        def create_embed(mode):
-            global index_count
+        embed = Embed()
+        embed.title = f"Definition for {word.lower()}"
+        embed.description = f"`[{part_of_speech.upper()}]` - {definition}"
 
-            if mode == 'next':
-                index_count += 1
-            elif mode == 'previous':
-                index_count -= 1
+        view = DefineWordPagination(word.lower(), ctx.author, data)
 
-            if index_count >= len(defs):
-                index_count = -1
-            elif index_count < 0:
-                index_count = 0
+        await ctx.send(embed=embed, view=view)
 
-            message = f"Here's what i got on the Word {word} \n\n {defs[index_count]}"
-            respond_message = Embed(
-                title=word,
-                description=message,
-                color=discord.Color.blurple()
-            )
-
-            return respond_message
-
-        async def callback_next(interaction: discord.Interaction):
-            respond_message = create_embed('next')
-            await interaction.response.edit_message(embed=respond_message, view=view)
-
-        async def callback_previous(interaction: discord.Interaction):
-            respond_message = create_embed('previous')
-            await interaction.response.edit_message(embed=respond_message, view=view)
-
-        button_next = Button(
-            label='Next', style=discord.ButtonStyle.green, emoji='➡')
-        button_previous = Button(
-            label='Previous', style=discord.ButtonStyle.green, emoji='⬅')
-        button_next.callback = callback_next
-        button_previous.callback = callback_previous
-        view = View()
-        view.add_item(button_previous)
-        view.add_item(button_next)
-
-        await ctx.send(embed=create_embed('first'), view=view)
-
-    # Command to Turn this Command On or Off
     @is_staff()
-    @command(name="toggle", description="Turn Define Command On/Off")
-    async def toggle(
-        self,
-        interaction: discord.Interaction,
-    ) -> None:
-        """
-        Turn Define Command On/Off
+    @command(name="toggle", description="Toggle the define word command.")
+    async def toggle_config(self, interaction: discord.Interaction):
+        """Toggles converter."""
 
-        :param interaction: Interaction
-        """
+        toggle_map = {
+            True: "ON",
+            False: "OFF"
+        }
+        toggle = await self.config.toggle_config("define_word")
+        await interaction.response.send_message(
+            f"Turned {toggle_map[toggle]} Define Word.",
+            ephemeral=True
+        )
 
-        if self.command_enabled:
-            self.command_enabled = False
-            response = "Command is now Disabled"
-        else:
-            self.command_enabled = True
-            response = "Command is now Enabled"
+    @staticmethod
+    def _format_data(data: List[dict]) -> List[Tuple[str, str]]:
+        formatted_data = []
 
-        await interaction.response.send_message(response, ephemeral=True)
+        while data:
+            temp = data.pop()
+            for meaning in temp["meanings"]:
+                for definition in meaning["definitions"]:
+                    formatted_data.append((meaning["partOfSpeech"], definition["definition"]))
+
+        return formatted_data
 
 
 async def setup(bot: Bot):
