3aa697c9cc25418c6cd16ee07c6add807dcab9aa
Jedddy <jedbalita25@gmail.com>
2023-04-14 22:26:23 +0800

    enhancement: improved selection view

--- bot/ui/views/auto_tag.py
+++ bot/ui/views/auto_tag.py
@@ -1,45 +1,72 @@
-from discord import Interaction, User, Role, ButtonStyle, ChannelType
+from discord import (
+    Interaction,
+    User,
+    Role,
+    ButtonStyle,
+    ChannelType
+)
 from discord.ui import (
     Button,
+    Modal,
     View,
     UserSelect,
     RoleSelect,
     ChannelSelect,
+    TextInput,
     button,
     select
 )
 
+from database.auto_tag import AutoTagDB
+
 
 class TaggingSelection(View):
-    def __init__(self):
-        self.selected: list[User | Role] = []
-        self.forum: int = None
+    def __init__(self, db: AutoTagDB):
+        self.db = db
+        self.selected: User | Role | None = None
+        self.forum: int | None = None
+        self.custom_msg: str = "Calling out peeps!"
         super().__init__()
 
-    @select(cls=UserSelect, placeholder="Select users...", max_values=25)
-    async def select_users(self, interaction: Interaction, selection: UserSelect):
-        self.selected.extend(selection.values)
+    async def _inadequate_check(self, interaction: Interaction):
+        if not self.forum:
+            await interaction.response.send_message(
+                "Please select a forum.",
+                ephemeral=True
+            )
+            return False
 
-        await interaction.response.send_message(
-            f"Selected {len(selection.values)} user(s).",
-            ephemeral=True
+        if not self.selected:
+            await interaction.response.send_message(
+                "Please select users/roles.",
+                ephemeral=True
+            )
+            return False
+
+        return True
+
+    @select(cls=UserSelect, placeholder="Select users...")
+    async def select_user(self, interaction: Interaction, selection: UserSelect):
+        self.selected = selection.values[0]
+
+        await interaction.response.edit_message(
+            content=f"Selected {self.selected.mention}",
+            view=self.remove_item(self.select_role)
         )
 
-    @select(cls=RoleSelect, placeholder="Select roles...", max_values=25)
-    async def select_roles(self, interaction: Interaction, selection: RoleSelect):
-        self.selected.extend(selection.values)
+    @select(cls=RoleSelect, placeholder="Select roles...")
+    async def select_role(self, interaction: Interaction, selection: RoleSelect):
+        self.selected = selection.values[0]
 
-        await interaction.response.send_message(
-            f"Selected {len(selection.values)} role(s).",
-            ephemeral=True
+        await interaction.response.edit_message(
+            content=f"Selected {self.selected.mention}",
+            view=self.remove_item(self.select_user)
         )
 
     @select(
         cls=ChannelSelect,
         placeholder="Select forum...",
-        channel_types=[ChannelType.forum],
-        min_values=1,
-        max_values=1
+        channel_types=[ChannelType.forum]
     )
     async def select_forum(self, interaction: Interaction, selection: RoleSelect):
         self.forum = selection.values[0].id
@@ -51,12 +78,26 @@ class TaggingSelection(View):
 
     @button(label="Submit", style=ButtonStyle.green)
     async def submit(self, interaction: Interaction, button: Button):
-        if not self.forum:
-            await interaction.response.send_message(
-                "Please select a forum.",
-                ephemeral=True
-            )
+        if not await self._inadequate_check(interaction):
             return
 
-        await interaction.response.edit_message(content="Done!")
+        async def callback(interaction: Interaction):
+            """The modal's callback so we don't get an error"""
+
+            self.custom_msg = msg.value
+            await interaction.response.edit_message(
+                content="Success.",
+                view=None
+            )
+
+        modal = Modal(title="Set Custom Message")
+        msg = TextInput(
+            label="Add custom message.",
+            placeholder="Type here...",
+            default=self.custom_msg
+        )
+        modal.on_submit = callback
+        modal.add_item(msg)
+        await interaction.response.send_modal(modal)
+        await modal.wait()
         self.stop()
