917039a38d234f4e213e6dbbe0975fc3d4e695d3
Jedddy <jedbalita25@gmail.com>
2023-04-17 23:25:52 +0800

    chore: remove posts table

--- migrations/5_anonymous_posting.sql
+++ migrations/5_anonymous_posting.sql
@@ -2,13 +2,6 @@ CREATE TABLE IF NOT EXISTS pph_anonymous_posting_forums (
     forum_id BIGINT PRIMARY KEY
 );
 
-CREATE TABLE IF NOT EXISTS pph_anonymous_posting_posts (
-    post_uuid uuid PRIMARY KEY,
-    post_id BIGINT,
-    post_author BIGINT,
-    post_title VARCHAR(100)
-);
-
 CREATE TABLE IF NOT EXISTS pph_anonymous_posting_log_channel (
     channel_id BIGINT PRIMARY KEY
 );
