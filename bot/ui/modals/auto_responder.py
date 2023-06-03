6b37578e100452219a12f24ab9301b03da8f8409
tora-o <97511605+tora-o@users.noreply.github.com>
2023-06-03 19:22:06 +0800

    Auto responder improvements (#146)
    * feat: created migration file for improvement
    
    * feat: created select and view for channel selection
    
    * enhancement: updated database handler for auto_responder
    
    * enhancement: implemented changes
    
    * feat: finished improving the command

--- bot/ui/modals/auto_responder.py
+++ bot/ui/modals/auto_responder.py
@@ -1,6 +1,8 @@
-from discord import Interaction, TextStyle
-from discord.ui import Modal, TextInput
 from database.auto_responder import AutoRespondDB
+from discord import Interaction, TextStyle
+from discord.ext.commands import Bot
+from discord.ui import Modal, TextInput
+from ui.views.auto_responder import AutoResponderSelect
 
 
 class AutoResponder(Modal, title="Auto Responder"):
@@ -16,16 +18,16 @@ class AutoResponder(Modal, title="Auto Responder"):
         style=TextStyle.paragraph
     )
 
-    def __init__(self, db: AutoRespondDB, response_type: str):
+    def __init__(self, db: AutoRespondDB, response_type: str, bot: Bot):
         self.db = db
+        self.bot = bot
         self.response_type = response_type
         super().__init__()
 
     async def on_submit(self, interaction: Interaction) -> None:
-        await self.db.insert_response(
-            self.message.value.strip().lower(),
-            self.response.value.strip(),
-            self.response_type
+        view = AutoResponderSelect(
+            self.db,
+            [channel for guild in self.bot.guilds for channel in guild.text_channels],
+            self
         )
-
-        await interaction.response.send_message("Success.", ephemeral=True)
+        await interaction.response.send_message(view=view, ephemeral=True)
---
+++ migrations/11_auto_responder_improvement.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS pph_auto_responder_channels (
+    id SERIAL PRIMARY KEY,
+    channel_id BIGINT NOT NULL,
+    response_id BIGINT NOT NULL,
+
+    FOREIGN KEY (response_id) REFERENCES pph_auto_responses(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+ALTER TABLE pph_auto_responses ADD COLUMN IF NOT EXISTS specified BOOLEAN NOT NULL DEFAULT FALSE;
