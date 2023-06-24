ed4be78f89acaca75c49f9e5da872033a9bc8885
Jhan Gale Garcia <garcia.johngale@gmail.com>
2023-06-24 20:14:16 +0800

    design changes & consistency

--- bot/utils/markdown_utils.py
+++ bot/utils/markdown_utils.py
@@ -6,18 +6,37 @@ def heading(text: str, level: int):
 
 
 def check_box(text: str, checked=False):
-    ternary = "[x]" if checked is True else "[ ]"
-    return f"- {ternary} {text}"
+    condition = "[x]" if checked is True else "[ ]"
+    return f"- {condition} {text}"
 
 
-def list_items(items):
+def bulleted_items(items):
     return "\n".join([f"- {text}" for text in items])
 
 
-def code_block(text: str, code_style: str, highlight=False):
-    ternary = f"```{code_style}\n{text} \n```" if highlight is True else f"```\n{text} \n```"
-    return f"{ternary}"
+def code_block_with_style(text: str, language: str):
+    return f"```{language}\n{text}\n```"
+
+
+def code_block(text: str):
+    return f"```\n{text}\n```"
 
 
 def inline_code(text: str):
     return f"`{text}`"
+
+
+def quote(text: str):
+    return f"> {text}"
+
+
+def block_quote(text: str):
+    return f">>> {text}"
+
+
+def hide_link_embed(url: str):
+    return f"<{url}>"
+
+
+def hyperlink(content: str, url: str):
+    return f"[{content}]({url})"
