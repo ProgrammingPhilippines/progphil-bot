e201181e9aeaa9aa068b6e1a7823e842ccb38bca
Jedddy <104408560+Jedddy@users.noreply.github.com>
2024-01-16 21:35:06 +0800

    fix: fetch threads if it's not in cache (#209)

--- bot/ui/views/dev_help.py
+++ bot/ui/views/dev_help.py
@@ -1,5 +1,6 @@
 from discord import ButtonStyle, Interaction, ForumChannel
 from discord.ui import View, Button, Select, button
+from discord.ui.item import Item
 
 from config import GuildInfo
 from database.dev_help import DevHelpViewsDB, DevHelpTagDB
@@ -12,7 +13,7 @@ class PersistentSolverView(View):
         author_id: int,
         db: DevHelpViewsDB,
         tag_db: DevHelpTagDB,
-        forum: ForumChannel
+        forum: ForumChannel,
     ):
         self.message_id = message_id
         self.author_id = author_id
@@ -25,11 +26,16 @@ class PersistentSolverView(View):
         conditions = (
             interaction.user.id == self.author_id,
             interaction.user.guild_permissions.administrator,
-            any(interaction.guild.get_role(role) in interaction.user.roles for role in GuildInfo.staff_roles)
+            any(
+                interaction.guild.get_role(role) in interaction.user.roles
+                for role in GuildInfo.staff_roles
+            ),
         )
 
         if not any(conditions):
-            await interaction.response.send_message("This isn't for you!", ephemeral=True)
+            await interaction.response.send_message(
+                "This isn't for you!", ephemeral=True
+            )
             return False
         return True
 
@@ -40,16 +46,21 @@ class PersistentSolverView(View):
         settings = await self.tag_db.get()
         thread = self.forum.get_thread(self.message_id)
 
+        if not thread:
+            thread = await self.forum.guild.fetch_channel(self.message_id)
+
         users = {
             (message.author.display_name, message.author.id)
             async for message in thread.history(limit=None)
-            if message.author.id != self.forum.guild.me.id and message.author.id != self.author_id
+            if message.author.id != self.forum.guild.me.id
+            and message.author.id != self.author_id
         }
 
         message = "This post has been marked as solved."
         view = None
 
         if users:
+
             async def selection_callback(interaction: Interaction):
                 await interaction.response.send_message("Thanks!", ephemeral=True)
                 view.stop()
@@ -59,7 +70,10 @@ class PersistentSolverView(View):
                 view.stop()
 
             view = View(timeout=60)
-            selection = Select(placeholder="Select users that helped you solve your problem.", max_values=len(users))
+            selection = Select(
+                placeholder="Select users that helped you solve your problem.",
+                max_values=len(users),
+            )
             cancel = Button(label="Cancel", style=ButtonStyle.red)
             cancel.callback = cancel_callback
             selection.callback = selection_callback
@@ -76,7 +90,7 @@ class PersistentSolverView(View):
             if selection.values:
                 members = [thread.guild.get_member(int(id)) for id in selection.values]
                 mentions = [member.mention for member in members if member is not None]
-                mentions_str = ', '.join(mentions)
+                mentions_str = ", ".join(mentions)
 
                 if len(mentions) > 1:
                     message += f" Thanks to the following users: {mentions_str}"
@@ -94,6 +108,19 @@ class PersistentSolverView(View):
         await thread.edit(locked=True)
         await thread.add_tags(tag, reason="Solved")
         await thread.edit(name=name)
-        
+
         await self.db.close_view(thread.id)
         self.stop()
+
+    async def on_error(
+        self,
+        interaction: Interaction,
+        error: Exception,
+        item: Item,
+    ) -> None:
+        log_channel = self.forum.guild.get_channel(GuildInfo.log_channel)
+
+        info = f"Thread Message ID: {self.message_id}"
+        await log_channel.send(
+            f"An error occurred while using the solve button:\n```{error}\n{info}```"
+        )
