1952b336b77560a3f507796cb33e210ebda2f193
Kyle Cancio <kylecancio@gmail.com>
2024-04-10 23:53:39 +0800

    @fix:Fix logger missing attribute

--- bot/utils/logging/logger.py
+++ bot/utils/logging/logger.py
@@ -4,12 +4,12 @@ from logging import Formatter, StreamHandler
 from config import LoggerConfig
 from utils.logging.discord_handler import DiscordHandler
 
+_stream_handler = StreamHandler()
 _discord_handler=DiscordHandler()
 logging.basicConfig(level=logging.getLevelName(LoggerConfig.log_level))
 
 # [2024-03-19 21:35:00] [INFO] [filepath.py] Sample log XYZ 
 _formatter = Formatter('[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s')
-_stream_handler = StreamHandler()
 _stream_handler.setFormatter(_formatter)
 _discord_handler.setFormatter(_formatter)
 
