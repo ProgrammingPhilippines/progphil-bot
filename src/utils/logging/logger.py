7c73b4f707e8c83339bb18a8c6845e4579edbd9e
mystique09 <knightkrypto7@gmail.com>
2024-07-10 01:27:42 +0800

    chore: remove comments

--- src/utils/logging/logger.py
+++ src/utils/logging/logger.py
@@ -8,17 +8,6 @@ from src.utils.logging.discord_handler import DiscordHandler
 
 # [2024-03-19 21:35:00] [INFO] [filepath.py] Sample log XYZ 
 _formatter = Formatter('[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s')
-# _stream_handler.setFormatter(_formatter)
-# _discord_handler.setFormatter(_formatter)
-#
-#
-# def getLogger(name):
-#     logger = logging.getLogger(name)
-#
-#     logger.addHandler(_stream_handler)
-#     logger.addHandler(_discord_handler)
-#
-#     return logger
 
 
 class BotLogger(object):
